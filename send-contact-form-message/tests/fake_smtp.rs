2029621a0d7dfd488b73bd91500c91d0b344edca
Bradford Hovinen <hovinen@gmail.com>
2023-11-13 21:25:34 +0100

    Move struct and impl outside function

--- send-contact-form-message/tests/fake_smtp.rs
+++ send-contact-form-message/tests/fake_smtp.rs
@@ -5,37 +5,39 @@ use tracing::debug;
 
 pub const SMTP_PORT: &str = "4567";
 
-pub fn setup_smtp() -> Receiver<String> {
-    #[derive(Clone)]
-    struct SmtpHandler(Vec<u8>, Arc<Mutex<Option<Sender<String>>>>);
-    impl Handler for SmtpHandler {
-        fn data(&mut self, buf: &[u8]) -> std::io::Result<()> {
-            debug!("Got data:\n{}", String::from_utf8_lossy(buf));
-            self.0.extend(buf);
-            Ok(())
-        }
+#[derive(Clone)]
+struct SmtpHandler(Vec<u8>, Arc<Mutex<Option<Sender<String>>>>);
 
-        fn data_end(&mut self) -> mailin_embedded::Response {
-            self.1
-                .lock()
-                .unwrap()
-                .take()
-                .unwrap()
-                .send(String::from_utf8(self.0.drain(..).collect()).unwrap())
-                .unwrap();
-            mailin_embedded::response::OK
-        }
-
-        fn auth_plain(
-            &mut self,
-            authorization_id: &str,
-            authentication_id: &str,
-            password: &str,
-        ) -> mailin_embedded::Response {
-            debug!("Got authentication data {authorization_id}, {authentication_id}, {password}");
-            mailin_embedded::response::AUTH_OK
-        }
+impl Handler for SmtpHandler {
+    fn data(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        debug!("Got data:\n{}", String::from_utf8_lossy(buf));
+        self.0.extend(buf);
+        Ok(())
     }
+
+    fn data_end(&mut self) -> mailin_embedded::Response {
+        self.1
+            .lock()
+            .unwrap()
+            .take()
+            .unwrap()
+            .send(String::from_utf8(self.0.drain(..).collect()).unwrap())
+            .unwrap();
+        mailin_embedded::response::OK
+    }
+
+    fn auth_plain(
+        &mut self,
+        authorization_id: &str,
+        authentication_id: &str,
+        password: &str,
+    ) -> mailin_embedded::Response {
+        debug!("Got authentication data {authorization_id}, {authentication_id}, {password}");
+        mailin_embedded::response::AUTH_OK
+    }
+}
+
+pub fn setup_smtp() -> Receiver<String> {
     let (tx, rx) = oneshot::channel();
     let handler = SmtpHandler(Vec::new(), Arc::new(Mutex::new(Some(tx))));
     let mut server = Server::new(handler);
